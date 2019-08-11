99bb3417901f5c7725b6ef22b16c7f40a49365c0
hyuri <hyuri.pimentel@gmail.com>
2019-08-11 04:10:16 -0300

    Dicts code style clean-up

--- code.py
+++ code.py
@@ -6,10 +6,29 @@ lists = ["red", "green", "blue"]
 # lists_of_lists = [["red", "green", "blue"], ["cyan", "magenta", "yellow"]]
 tuples = (300, 200, 100)
 
-dicts = {"one": 1, "two": 2, "three": 3}
-translation = {"x": 0, "y": 0, "z": 0}
-rotation = {"x": 0, "y": 0, "z": 0}
-scale = {"x": 0, "y": 0, "z": 0}
+dicts = {
+	"one": 1,
+	"two": 2,
+	"three": 3
+}
+
+translation = {
+	"x": 0,
+	"y": 0,
+	"z": 0
+}
+
+rotation = {
+	"x": 0,
+	"y": 0,
+	"z": 0
+}
+
+scale = {
+	"x": 0,
+	"y": 0,
+	"z": 0
+}
 
 UI_strings = strings
 UI_integers = integers
